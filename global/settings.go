ff0fa481315570cf72458d1195aeca4ac486c4fa
halweg <8688534haowei@gmail.com>
2020-10-27 00:40:39 +0800

    add logger pkg

--- global/settings.go
+++ global/settings.go
@@ -1,9 +1,13 @@
 package global
 
-import "github.com/halweg/gin-blog/pkg/settings"
+import (
+    "github.com/halweg/gin-blog/pkg/logger"
+    "github.com/halweg/gin-blog/pkg/settings"
+)
 
 var (
     ServerSettings *settings.ServerSettings
     AppSettings *settings.AppSettings
     DatabaseSettings *settings.DataBaseSettings
+    Logger *logger.Logger
 )
